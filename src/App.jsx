307c99c52a4e692d176033332b355fe8feac10ed
Renato Ferraz <renato.ferraz@qodeless.io>
2025-05-02 14:50:27 -0300

    Instalar e configurar o Biome

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-function App() {
+import { Post } from "./Post";
+
+export function App() {
   return (
-    <>
-        <h1>Hello World</h1>
-    </>
+    < div>
+      <h1>App</h1>
+      <Post
+        autor="João"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+          Eius natus nisi assumenda possimus beatae omnis iure distinctio, 
+          amet tempore quae corrupti totam, ad illum cumque perferendis eos 
+          voluptate dignissimos id."
+      />
+    </div>
   )
 }
-
-export default App
